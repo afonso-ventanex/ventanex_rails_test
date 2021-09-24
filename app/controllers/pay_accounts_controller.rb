644f9e9175514530975f14c6e2e0e34e29f800fa
Afonso Arantes <afonso.h.arantes@gmail.com>
2021-09-23 20:28:53 -0500

    Finished code

--- app/controllers/pay_accounts_controller.rb
+++ app/controllers/pay_accounts_controller.rb
@@ -4,6 +4,16 @@ class PayAccountsController < ApplicationController
 
   def index
     @pay_accounts = PayAccount.all
+
+    loan_file = File.read(Rails.root + "app/assets/xml/loan.xml")
+    parsed = Nokogiri::XML(loan_file)
+
+    @loan_number = parsed.xpath("//LoanNumber").collect(&:text).first
+    @borrower_name = parsed.xpath("//BorrowerFullNameUnformated").collect(&:text).first
+  end
+
+
+  def new
   end
 
   def create
